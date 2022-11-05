6b5099478c20da35c5e84bf8ccd5d41c89db5005
Zelu Tesema <zelu.tesema@labs.epiuse.com>
2022-11-05 19:53:25 +0200

    Zelu changes so far

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,106 @@
-function clicked(value){
-    if(value==1)
-    {
-        let card1 = document.querySelector('#card1')
-        alert("Hi, "+card1.innerHTML)
+let numSorted = 0
+function selectLargest(value){
+  let maxMoves = getNumberVisibleUnsorted() - 1
+  let currentCounter = parseInt(document.getElementById("moveCounter").innerHTML) + 1
+  document.getElementById("moveCounter").innerHTML = ""
+  document.getElementById("moveCounter").innerHTML = currentCounter.toString()
+
+
+  let selectedCardID = "card" + value
+  let card1 =  document.getElementById("card1")
+  let card2 = document.getElementById("card2")
+  card1.innerHTML = document.getElementById(selectedCardID).innerHTML
+
+  card2.innerHTML = ""
+
+  setSelectedBorderDefault()
+  setSelectedBorder(card1.innerHTML, card2.innerHTML)
+
+  if(currentCounter === maxMoves){
+    insertCard1IntoSorted()
+    hideCard1FromUnsorted()
+    setCard1FromUnsorted()
+  }
+}
+
+function setCard1FromUnsorted(){
+  let unsortedGridChildren = document.getElementById("unsortedList").children
+  let card1 =  document.getElementById("card1")
+
+  for(let i = 1 ; i < unsortedGridChildren.length ; i++){
+    if(unsortedGridChildren[i].display != "none"){
+      card1.innerHTML = unsortedGridChildren[i].innerHTML
+      return
     }
-    else{
-        let card2 = document.querySelector('#card2')
-        alert("Hi, "+card2.innerHTML)
+  }
+}
+function hideCard1FromUnsorted(){
+  let unsortedGrid = document.getElementById("unsortedList")
+  let unsortedGridChildren = unsortedGrid.children
+
+  for(let i = 0 ; i < unsortedGridChildren.length ; i++){
+    if(unsortedGridChildren[i].innerHTML == 1){
+      unsortedGridChildren[i].style.display = "none"
     }
+  }
+}
+
+function insertCard1IntoSorted(){
+  let unsortedGrid = document.getElementById("sortedList")
+  let unsortedGridChildren = unsortedGrid.children
+  let card1Value = document.getElementById("card1").innerHTML
+
+  unsortedGridChildren[numSorted+1].style.display = "inline"
+  unsortedGridChildren[numSorted+1].innerHTML = card1Value
+  return
+
+}
+
+function selectUnsortedItem(index){
+  let id = "sortedItem_" + index;
+  let selectedNumber = document.getElementById(id).getInnerHTML();
+  let fillSection = document.getElementById("card2").innerHTML = selectedNumber
+
+  let card1Value = document.getElementById("card1").innerHTML
+
+  setSelectedBorderDefault()
+  setSelectedBorder(card1Value, fillSection)
+}
+
+function setSelectedBorderDefault(){
+  let unsortedGrid = document.getElementById("unsortedList")
+  let unsortedGridChildren = unsortedGrid.children
+
+  for(let i = 0 ; i < unsortedGridChildren.length ; i++){
+      unsortedGridChildren[i].style.border = ""
+  }
+}
+
+function setSelectedBorder(value1,value2){
+  let unsortedGrid = document.getElementById("unsortedList")
+  let unsortedGridChildren = unsortedGrid.children
+
+  for(let i = 0 ; i < unsortedGridChildren.length ; i++){
+    if (unsortedGridChildren[i].innerHTML == value1){
+      unsortedGridChildren[i].style.border = "3px solid salmon"
+    }
+
+    else if(unsortedGridChildren[i].innerHTML == value2){
+      unsortedGridChildren[i].style.border = "3px solid lightskyblue"
+    }
+  }
+}
+
+function getNumberVisibleUnsorted(){
+  let unsortedGrid = document.getElementById("unsortedList")
+  let unsortedGridChildren = unsortedGrid.children
+
+  let numVisibleSorted = 0
+  for(let i = 0 ; i < unsortedGridChildren.length ; i++){
+    if (unsortedGridChildren[i].style.display != "none"){
+      numVisibleSorted +=1
+    }
+  }
+
+  return numVisibleSorted-1
 }
